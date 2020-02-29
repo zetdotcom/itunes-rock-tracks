7af6fed1b9b1104b06a11f348705dd96fd55d863
Michal Zet <michal.zadarnowski@gmail.com>
2020-02-29 22:38:46 +0000

    Updates

---
+++ precache-manifest.035b4b7123b7022a6d0b88847e4026e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ff7666044fc1156858bb90d57d7c29f",
+    "url": "/itunes-rock-tracks/index.html"
+  },
+  {
+    "revision": "e7756c90c7ba2c5ccae2",
+    "url": "/itunes-rock-tracks/static/css/main.dc6dcdf0.chunk.css"
+  },
+  {
+    "revision": "2a398a164f5e47a8b615",
+    "url": "/itunes-rock-tracks/static/js/2.edc9eca4.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/itunes-rock-tracks/static/js/2.edc9eca4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7756c90c7ba2c5ccae2",
+    "url": "/itunes-rock-tracks/static/js/main.9111e263.chunk.js"
+  },
+  {
+    "revision": "a7fc14c204fbb25bfb9f",
+    "url": "/itunes-rock-tracks/static/js/runtime-main.00c5cd5b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8a1a6cf7e61aebcbcadfd61962f39994.js"
+  "/itunes-rock-tracks/precache-manifest.035b4b7123b7022a6d0b88847e4026e2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/itunes-rock-tracks/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
